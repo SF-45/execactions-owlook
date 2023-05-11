7fbe94e4107a2b89277dd35812a5b0d8789e3a08
SF-45 <akcaynt1@gmail.com>
2023-05-11 11:42:05 +0700

    Доработка JAXBEntity, теперь можно отслеживать события добавления и удаления сущностей.

--- src/main/java/space/sadfox/execactions/windows/ExecCommandEditNode.java
+++ src/main/java/space/sadfox/execactions/windows/ExecCommandEditNode.java
@@ -6,8 +6,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import space.sadfox.dataccess.command.CommandEntity;
 import space.sadfox.owlook.ui.base.Controller;
 
 public class ExecCommandEditNode extends Controller {
@@ -29,8 +27,6 @@ public class ExecCommandEditNode extends Controller {
     
     @FXML
     RadioButton cmdShell;
-    
-    CommandEntity commandEntity;
     
 
 	ExecCommandEditNode() throws IOException {
